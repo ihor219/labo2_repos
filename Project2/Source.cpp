e3b63e6f96010dc21a426af00aeb8ea4270efb55
ihor <ihor.horbatyi.ik.2022@lpnu.ua>
2022-10-14 09:22:09 +0300

    first master commit

---
+++ Project2/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<math.h>
+
+using namespace std;
+int main()
+{
+	double a= 7; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "a = "; cin >> a;
+	z1 = (sin (a) + sin (5*a) - sin (3*a))/((cos)(a) - cos (3*a) + cos (5*a));
+	z2 = tan (3*a);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
